ed27be0003f7009b5669bc2052faaad7e008a016
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-05-04 13:34:54 +0100

    Fix project

---
+++ desktop/src/com/groundcontrol/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.groundcontrol.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.groundcontrol.game.GroundControl;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GroundControl(), config);
+	}
+}
